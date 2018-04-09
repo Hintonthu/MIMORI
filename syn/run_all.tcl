f6ad5a845842526f3c54eca6d3401739c1b44e45
johnjohnlin <johnjohnlin@gmail.com>
2018-04-10 02:11:07 +0800

    Make script more verbose

--- syn/run_all.tcl
+++ syn/run_all.tcl
@@ -1,5 +1,11 @@
+echo "setup..."
 source ./read_all.tcl > ./rpt/00_setup.txt
+echo "ungroup..."
 source ./ungroup.tcl > ./rpt/05_ungroup.txt
+echo "link & check..."
 source ./link.tcl > ./rpt/10_link.txt
+echo "compile..."
 source ./compile.tcl > ./rpt/15_compile.txt
+echo "output..."
 source ./write_all.tcl > ./rpt/20_report.txt
+echo "done"
